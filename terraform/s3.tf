98be6933682e1dc482785dce809790055d413655
jonnyCodev <42693227+jonnyCodev@users.noreply.github.com>
2022-02-21 12:15:26 +0200

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,6 +3,8 @@ resource "aws_s3_bucket" "data" {
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
+  
+  
   bucket        = "${local.resource_prefix.value}-data"
   acl           = "public-read"
   force_destroy = true
@@ -28,6 +30,8 @@ resource "aws_s3_bucket" "financials" {
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
+  
+  
   
   bucket        = "${local.resource_prefix.value}-financials"
   acl           = "private"
@@ -56,10 +60,6 @@ resource "aws_s3_bucket" "operations" {
 
 resource "aws_s3_bucket" "data_science" {
   # bucket is not encrypted
-  
-  
-  
-  
   
   bucket = "${local.resource_prefix.value}-data-science"
   acl    = "private"
